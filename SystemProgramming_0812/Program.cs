2cacee3b73f32242ed139efa540b93caac4988e4
Cefe_a8xp@itstep.edu.az <Cefe_a8xp@itstep.edu.az>
2021-08-12 17:46:37 +0400

    Add project files.

---
+++ SystemProgramming_0812/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace SystemProgramming_0812
+{
+    class Program
+    {
+        public volatile int a;
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>();
+            num.AsParallel().AsOrdered().ToList();
+            
+            Timer t = new Timer((c) =>
+            {
+                //var l = c as List<int>;
+                Console.WriteLine("Hello");
+            }, null, 1000, 500);
+            Console.ReadLine();
+            //t.Dispose();
+            //Console.ReadLine();
+        }
+    }
+}
